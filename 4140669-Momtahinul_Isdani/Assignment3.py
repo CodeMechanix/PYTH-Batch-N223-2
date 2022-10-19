6d4107fd5dd077dd27407112febe67647791615d
isdani <momtahinul.isdani@gmail.com>
2022-10-19 20:21:56 +0600

    updated the assignment3 file

--- 4140669-Momtahinul_Isdani/Assignment3.py
+++ 4140669-Momtahinul_Isdani/Assignment3.py
@@ -3,6 +3,16 @@
 """
 
 lst = list(map(int, input("Enter Integer numbers separated by space: ").split()))
+
+# using lambda filter
+print("# using lambda filter")
+evenCount = len(list(filter(lambda num: (num % 2 == 0), lst)))
+oddCount = len(list(filter(lambda num: (num % 2 == 1), lst)))
+print("Total Even: ", evenCount, "\nTotal Odd: ", oddCount)
+
+
+# using comprehension
+print("# using comprehension")
 evenCount = len([num for num in lst if num % 2 == 0])
 oddCount = len([num for num in lst if num % 2 == 1])
 print("Total Even: ", evenCount, "\nTotal Odd: ", oddCount)
